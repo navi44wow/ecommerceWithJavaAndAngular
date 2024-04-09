ba48822b9d666879018a5cdff9cfd360762de046
User <ivan.belev69@gmail.com>
2024-04-09 16:15:54 +0300

    updating the stripe public key wit the proper one;

--- 03-frontend/angular-ecommerce/src/environments/environment.development.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   luv2shopApiUrl: 'https://localhost:8443/api',
   stripePublishableKey:
-    'pk_test_51P1Ri6DiDTPfOTJehAajoVMI0iBGaq0TlLqzBiBtnRDBtag1ML5I9KSKhcAWVIEmx69bOq018BxTAGqWsvHidJ3Y000T3ocfUl',
+    'pk_test_51P1vfJJh3uSChZmFjpA4b88CutkYYREgtFCRnsv12sLH6nW11JtjzCXnVzXq38nFWu8fmq3A9TN3DDCQeM0GJDZe00CDnYW7nQ',
 };
